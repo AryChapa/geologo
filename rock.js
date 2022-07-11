45b5e07fcadcb98e03b369000be6223a6c8fa342
AryChapa <93797453+AryChapa@users.noreply.github.com>
2022-07-11 11:03:33 -0600

    Add files via upload

---
+++ rock.js
@@ -0,0 +1,28 @@
+class Rock {
+    constructor(x,y,width,height) {
+      var options = {
+        'density':7,
+        'friction': 3.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y,width ,height , options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('blue');
+      fill('green');
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
